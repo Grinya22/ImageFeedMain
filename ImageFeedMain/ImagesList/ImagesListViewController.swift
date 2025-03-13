6136d07d79e414107a95b5adf9855ea184c8b1f2
Grinya <gregory.vanyurin@icloud.com>
2025-03-13 20:11:32 +0400

    tabbar and navigation, profile screen, image to full screen and zoom
    I made a tabbar and navigation, added a profile screen, and also made the image open to the full screen with a share button and zoom

--- ImageFeedMain/ImagesList/ImagesListViewController.swift
+++ ImageFeedMain/ImagesList/ImagesListViewController.swift
@@ -1,11 +1,12 @@
 import UIKit
 
 class ImagesListViewController: UIViewController {
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
     
     @IBOutlet private var tableView: UITableView!
     
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
-
+    
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
@@ -16,11 +17,28 @@ class ImagesListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        tableView.delegate = self
-//        tableView.dataSource = self
+        //        tableView.delegate = self
+        //        tableView.dataSource = self
         
         tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left:0, bottom: 12, right: 0)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier {
+            guard
+                let viewController = segue.destination as? SingleImageViewController,
+                let indexPath = sender as? IndexPath
+            else {
+                assertionFailure("Invalid segue destination")
+                return
+            }
+            
+            let image = UIImage(named: photosName[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
 }
 
@@ -32,7 +50,7 @@ extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { // возвращает ячейку
         let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
         
-//        Возвращаемая ячейка из метода выше имеет тип UITableViewCell. Но нам нужно работать именно с нашей кастомной ячейкой ImagesListCell, чтобы получить доступ к её специфичным свойствам и методам. Поэтому используется конструкция guard let, которая пытается привести ячейку к типу ImagesListCell.
+        //        Возвращаемая ячейка из метода выше имеет тип UITableViewCell. Но нам нужно работать именно с нашей кастомной ячейкой ImagesListCell, чтобы получить доступ к её специфичным свойствам и методам. Поэтому используется конструкция guard let, которая пытается привести ячейку к типу ImagesListCell.
         
         guard let imageListCell = cell as? ImagesListCell else {
             return UITableViewCell()
@@ -44,7 +62,7 @@ extension ImagesListViewController: UITableViewDataSource {
         return imageListCell
     }
     
-//    По такому принципу этот метод работает для любой таблицы. Сначала нам нужно получить ячейку для определённой секции и позиции в секции, далее — привести её к нужному типу, чтобы работать с ячейкой, сконфигурировать её и вернуть ячейку из метода.
+    //    По такому принципу этот метод работает для любой таблицы. Сначала нам нужно получить ячейку для определённой секции и позиции в секции, далее — привести её к нужному типу, чтобы работать с ячейкой, сконфигурировать её и вернуть ячейку из метода.
     
 }
 
@@ -58,18 +76,20 @@ extension ImagesListViewController {
         cell.cellImage.image = image
         
         cell.dataLabel.text = dateFormatter.string(from: Date())
-//
-//        if indexPath.row % 2 == 0 {
-//            cell.likeButton.setImage(UIImage(named: "like_button_on"), for: .normal)
-//        } else {
-//            cell.likeButton.setImage(UIImage(named: "like_button_off"), for: .normal)
-//        }
-//
+        //
+        //        if indexPath.row % 2 == 0 {
+        //            cell.likeButton.setImage(UIImage(named: "like_button_on"), for: .normal)
+        //        } else {
+        //            cell.likeButton.setImage(UIImage(named: "like_button_off"), for: .normal)
+        //        }
+        //
     }
 }
 
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
+    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
@@ -77,16 +97,17 @@ extension ImagesListViewController: UITableViewDelegate {
         }
         
         let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
-
+        
         let imageWidth = image.size.width
         let imageHeight = image.size.height
-
+        
         let cellWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
         let scale = cellWidth / imageWidth
-
+        
         let cellHeight = imageHeight * scale + imageInsets.top + imageInsets.bottom
-
+        
         return cellHeight
-
+        
     }
 }
+
---
+++ ImageFeedMain/Profile/ProfileViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var loginNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    @IBAction func didTapLogoutButton(_ sender: Any) {
+    }
+}
