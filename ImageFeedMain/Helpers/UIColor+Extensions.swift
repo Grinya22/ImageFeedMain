0283a84588844cf85acf4f4554a1f45ae712269c
Grinya <gregory.vanyurin@icloud.com>
2025-04-28 15:40:26 +0400

    colors fixed

--- ImageFeedMain/Helpers/UIColor+Extensions.swift
+++ ImageFeedMain/Helpers/UIColor+Extensions.swift
@@ -1,7 +1,5 @@
 import UIKit
 
-extension UIColor { }
-
 extension UIColor {
     static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
     static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
@@ -11,17 +9,3 @@ extension UIColor {
     static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
     static var ypBlue: UIColor { UIColor(named: "YP Blue") ?? UIColor.blue }
 }
-
-//YP Black (iOS) 1A1B22
-
-//YP Gray (iOS) AEAFB4
-
-//YP Background (iOS) 1A1B22 (Alpha 50)
-
-//YP White (iOS) FFFFFF
-
-//YP White (Alpha 50) (iOS) FFFFFF (Alpha 50)
-
-//YP Red (iOS) F56B6C
-
-//YP Blue (iOS) 3772E7
